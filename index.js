479eb5e60d40190a14c0539a9a7b986520df51b8
2eg4rizve <2eg4rizve.com>
2023-08-19 20:21:01 +0600

    3 cards

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("index.js");
 let count = 0;
-let sum = 0;
+let sum = 0;// without discount price
+let total = 0;//last price
 
 
 // card 1
@@ -29,10 +30,96 @@ document.getElementById('card-1').addEventListener('click', function () {
     const SumPrice = document.getElementById('sum-p');
     SumPrice.innerHTML = sum;
 
-
+    total=sum;
 
     const lastPrice = document.getElementById('last-p');
-    lastPrice.innerHTML = sum;
+    lastPrice.innerHTML = total;
+
+
+
+
+
+
+
+
+})
+
+
+
+// card 2
+document.getElementById('card-2').addEventListener('click', function () {
+    console.log("card 2 click");
+
+    count++;
+
+    const c2Price = document.getElementById('card-2-price').innerText;
+    console.log(c2Price);
+
+    const output = document.getElementById('output');
+    const p = document.createElement('p');
+
+    const name = document.getElementsByClassName('card2-name');
+
+    console.log(name);
+
+
+    p.innerHTML = `${count} . K. Accessories`;
+    output.appendChild(p);
+
+    sum = parseFloat(sum) + parseFloat(c2Price);
+    console.log(sum);
+
+    const SumPrice = document.getElementById('sum-p');
+    SumPrice.innerHTML = sum;
+
+    total=sum;
+
+    const lastPrice = document.getElementById('last-p');
+    lastPrice.innerHTML = total;
+
+
+
+
+
+
+
+
+})
+
+
+
+// card 3
+
+document.getElementById('card-3').addEventListener('click', function () {
+    console.log("card 3 click");
+
+    count++;
+
+    const c3Price = document.getElementById('card-3-price').innerText;
+    console.log(c3Price);
+
+    const output = document.getElementById('output');
+    const p = document.createElement('p');
+
+    const name = document.getElementsByClassName('card3-name');
+
+    console.log(name);
+
+
+    p.innerHTML = `${count} . Home Cooker`;
+    output.appendChild(p);
+
+    sum = parseFloat(sum) + parseFloat(c3Price);
+    console.log(sum);
+
+    const SumPrice = document.getElementById('sum-p');
+    SumPrice.innerHTML = sum;
+
+
+    total = sum;
+
+    const lastPrice = document.getElementById('last-p');
+    lastPrice.innerHTML = total;
 
 
 
